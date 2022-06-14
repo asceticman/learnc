edf8c6e6d5f5e94a1e9767a9d8be446f9ea0b038
andy <andywhite0119@gmail.com>
2022-06-14 17:15:36 +0800

    add the overflow situation of int type

--- c_primer_plus_practise/chapter2_overflow.c
+++ c_primer_plus_practise/chapter2_overflow.c
@@ -1,1 +1,9 @@
 /* chapter2_overflow.c -- check out overflow on various types*/
+#include<stdio.h>
+int main(void)
+{
+    unsigned int uni_a =4294967295;
+    printf("Overflow of unsigned int,display the maximum int number 4294967295 plus 1, and the reslut is %u \n",uni_a+1);
+    printf("Overflow of float,display the maximum float number  ")
+    return 0;
+}
